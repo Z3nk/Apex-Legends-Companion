2bf5a8f253a5edb6edec4e8050b0db20ab9d6a50
damien GAILLARD <dgaillard@norsys.fr>
2019-02-12 11:38:49 +0100

    feat: add fabric

--- app/src/main/java/fr/apexlegends/guide/home/MainActivity.kt
+++ app/src/main/java/fr/apexlegends/guide/home/MainActivity.kt
@@ -5,11 +5,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import fr.apexlegends.guide.R
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 
